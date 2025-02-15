7f58300886599ab90e1759f805acb8e2a18a0418
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 12:56:09 -0500

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,6 @@
+# This file makes the database directory a Python package.
+# It can be left empty or used to expose specific functions/classes.
+from .db_handler import save_game_data, save_picks, get_historical_data
+from .models import GameData, Picks, HistoricalData
 
+__all__ = ['save_game_data', 'save_picks', 'get_historical_data', 'GameData', 'Picks', 'HistoricalData']
