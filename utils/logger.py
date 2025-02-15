1f4a2cbbf7a29674aaa62e80f8936405976ebdcb
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 12:49:57 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,16 @@
+import logging
+import os
 
+def setup_logger():
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("logs/game_picker.log"),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger(__name__)
