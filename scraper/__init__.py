fc3bf6365d003c11545aa287765a0a4864365c60
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 12:53:26 -0500

    Update __init__.py

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,1 +1,7 @@
 
+# This file makes the scraper directory a Python package.
+# It can be left empty or used to expose specific functions/classes.
+from .web_scraper import fetch_game_data
+from .data_parser import parse_game_data
+
+__all__ = ['fetch_game_data', 'parse_game_data']
