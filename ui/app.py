c5e0372dad21c3e75010fcf7afc97951b0f0be0d
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 12:45:13 -0500

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
+from database.db_handler import get_historical_data
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results')
+def results():
+    historical_data = get_historical_data()
+    return render_template('results.html', data=historical_data)
+
+def run_ui():
+    app.run(debug=True)
