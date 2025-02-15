1fda42a19bf8767f09e9764aa2e44d36c0e43211
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 12:58:27 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
+# This file makes the utils directory a Python package.
+# It can be left empty or used to expose specific functions/classes.
+from .logger import setup_logger
+from .config import DATABASE_URI
 
+__all__ = ['setup_logger', 'DATABASE_URI']
